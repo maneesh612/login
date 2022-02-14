4a208106b2a3e88efc57e03e695f65d597e53b85
maneesh612 <64636896+maneesh612@users.noreply.github.com>
2022-02-14 13:20:58 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,14 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBk81PEdEqle5Cc2NkAbCzL4YHICGOUtQY",
+  authDomain: "visitorpass0212.firebaseapp.com",
+  databaseURL: "https://visitorpass0212-default-rtdb.firebaseio.com",
+  projectId: "visitorpass0212",
+  storageBucket: "visitorpass0212.appspot.com",
+  messagingSenderId: "906310805547",
+  appId: "1:906310805547:web:bdd49cea6d781fd20d626e",
+  measurementId: "G-L7NX41HCFX"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
